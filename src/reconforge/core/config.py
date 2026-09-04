48c8b870233b161f894054a026b434cba8e009fd
CyprienThuillier <208813283+CyprienThuillier@users.noreply.github.com>
2026-09-04 23:26:54 +0200

    fix(config): fixing the default value for ports and the output path in Config

--- src/reconforge/core/config.py
+++ src/reconforge/core/config.py
@@ -1,12 +1,13 @@
-from .models import Target
-from .enums import Modules
-
 from pathlib import Path
 
-class Config():
-    def __init__(self, target:str, modules:list[Modules], ports:list[str] | [], wordlist:Path | None, output:Path | None):
+from .enums import Modules
+from .models import Target
+
+
+class Config:
+    def __init__(self, target:str, modules:list[Modules], ports:list[str] | None, wordlist:Path | None, output:Path | None):
         self.target = Target(target)
         self.modules = [Modules(module) for module in modules]
-        self.ports = ports
+        self.ports = ports if ports is not None else []
         self.wordlist = wordlist
-        self.output = output
+        self.output = Path(output) if output else None
